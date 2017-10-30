aff33a62359ed3f7b3228cdc4bac521c291aaf19
github username <github email address>
2017-10-30 17:58:08 -0400

    Done.

--- app.rb
+++ app.rb
@@ -14,5 +14,16 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get "/goodbye/:name" do
+    @new_user = params[:name]
+    "Goodbye, #{@new_user}."
+  end
+
+  get '/multiply/:num1/:num2' do
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    "#{@num1 * @num2}"
+  end
+
 
 end
